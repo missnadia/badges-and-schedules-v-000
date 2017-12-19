7eddb8079cf6acfb592a08025f7e4bbf90a0504b
Nadia Lee <miss.nadia.lee@gmail.com>
2017-12-19 20:25:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each.with.index(1).map do |attendee, index|
+  attendees.each.with_index(1).map do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
 end
