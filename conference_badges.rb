b96dd4acf4cc47fb704d3125a3dabc9b82747f25
Nadia Lee <miss.nadia.lee@gmail.com>
2017-12-19 20:27:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,6 +16,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each { |badge| puts "#{badge}" }
+  batch_badge_creator(attendees).each { |badge| puts badge }
   assign_rooms(attendees).each { |attendee| puts "#{attendee}" }
 end
