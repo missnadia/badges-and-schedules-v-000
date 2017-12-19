ea122c8f90c7108e397c7d4bf59fcb6e5739f2a5
Nadia Lee <miss.nadia.lee@gmail.com>
2017-12-19 20:13:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,12 @@
 # Write your code here.
 def batch_badge_creator(attendees=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
-  attendees.each { |name| puts "Hello, my name is #{name}."}
+  attendees.each { |name| return "Hello, my name is #{name}."}
 end
 
 def assign_rooms(room_assignments)
-
-  puts"Hello, #{name}.! You'll be assigned to room #{room}!"
+  room_assignments.each.with.index(1) do |value, index|
+    return "Hello, #{name}.! You'll be assigned to room #{room}!"
+  end
 end
 
 def printer
