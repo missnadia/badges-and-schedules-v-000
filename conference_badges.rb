20fb68500966b38567a664cd1d87b8e6036de9cf
Nadia Lee <miss.nadia.lee@gmail.com>
2017-12-19 20:00:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+def batch_badge_creator(attendees=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+  attendees.each { |name| puts "Hello, my name is #{name}."}
+end
+
+def assign_rooms(room)
+  puts"Hello, #{name}.! You'll be assigned to room #{room}!"
+end
